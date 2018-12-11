484f0c7d0c2fbf5a666ee6d9edacc3e5b6748363
petro.bishko <petro.bishko.uk@gmail.com>
2018-12-11 23:17:24 +0200

    absolute values sum minization

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,12 @@
 function absoluteValuesSumMinimization(a: number[]): number {
+  // check if length of numbers is even or odd
+  const isEven = a.length % 2 === 0;
 
+  // return medium value if there few return smaller one
+  return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length - 2)];
 }
 
 // console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
