ee51d88d3e95215e993cb0f334ddf9b10d35c7bf
petro.bishko <petro.bishko.uk@gmail.com>
2018-12-13 18:17:18 +0200

    add Border

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,27 @@
 function add(param1: number, param2: number): number {
-
+  return param1 + param2;
 }
 
-// function add2(param1: number[]): number {
+// used for loop
+function add2(...params: number[]): number {
+  let total: number = 0;
+  for (let key of params) {
+    total += key;
+  }
+  return total;
+}
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+// use reduce method
+function add3(...params: number[]): number {
+  return params.reduce((accumulator, currentValue) => accumulator + currentValue);
+}
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add(1, 2));
+console.log(add(3, 2));
+
+console.log(add2(1, 2, 3, 4, 5));
+console.log(add2(2, 3));
+
+console.log(add3(1, 2, 3, 4, 5, 1));
+console.log(add3(2, 3, 1));
 
--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,19 @@
 function addBorder(picture: string[]): string[] {
+  const lengthOfWall = picture[0].length + 2;
 
+  let wall = '';
+  for (let i = 0; i < lengthOfWall; i++) {
+    wall = wall.concat('*');
+  }
+
+  for (let k in picture) {
+    picture[k] = `*${picture[k]}*`;
+  }
+
+  picture.unshift(wall);
+  picture.push(wall);
+
+  return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
