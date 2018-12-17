082297c0caf10334537f3ab44ed1bb23cecbccb2
petro.bishko <petro.bishko.uk@gmail.com>
2018-12-17 10:25:07 +0200

    longest string

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,7 @@
 function addTwoDigits(n: any): number {
+  const nums = n.toString().split('');
 
+  return nums.reduce((a: string, b: string) => parseInt(a) + parseInt(b));
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,16 @@
 function allLongestStrings(inputArray: string[]): string[] {
+  let longerWordsLength = 0;
 
+  const arrayStrings = [];
+
+  for (let item of inputArray) {
+    longerWordsLength = longerWordsLength < item.length ? item.length : longerWordsLength;
+
+    if (item.length === longerWordsLength) {
+      arrayStrings.push(item);
+    }
+  }
+  return arrayStrings;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
