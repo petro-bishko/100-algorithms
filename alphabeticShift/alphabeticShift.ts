3dc6b23bb7b0f95726489a26f029c7adb3175b72
petro.bishko <petro.bishko.uk@gmail.com>
2018-12-26 12:06:07 +0200

    alphabeticShift done

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,17 @@
 function alphabeticShift(inputString: string): string {
+  const alphabet: string[] = ['a', 'b', 'c', 'd', '...'];
 
+  let inputShifted = inputString.split('');
+
+  for (let i = 0; i < inputString.length; i++) {
+    let index = 0;
+    if (inputShifted[i] !== 'z') {
+      index = alphabet.indexOf(inputShifted[i]) + 1;
+    }
+    inputShifted[i] = alphabet[index];
+  }
+
+  return inputShifted.join('');
 }
 
 console.log(alphabeticShift('crazy'));
