3b46f404948aaccb075a88989d44874dc15ff63d
petro.bishko <petro.bishko.uk@gmail.com>
2018-12-26 12:18:33 +0200

    alphabetSubSequence done

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,27 @@
 function alphabetSubsequence(s: string): boolean {
+  const chars: string[] = s.split('');
+  const charValue: number[] = [];
+
+  chars.forEach((char: string) => {
+    charValue.push(char.charCodeAt(0));
+  });
+
+  if (new Set(charValue).size !== charValue.length) {
+    return false;
+  }
+
+  for (let i = 0; i < charValue.length - 1; i++) {
+    if (charValue[i] >= charValue[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
 
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence('zab'));
+console.log(alphabetSubsequence('effg'));
+console.log(alphabetSubsequence('cdce'));
+console.log(alphabetSubsequence('ace'));
+console.log(alphabetSubsequence('bxz'));
